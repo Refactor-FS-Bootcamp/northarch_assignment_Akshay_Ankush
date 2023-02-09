a870712455b2cca7c934289f667b97abc93a8d64
hpmeAkshay <akshaykad.51@gmail.com>
2023-02-09 22:08:50 +0530

    Completed All task

---
+++ src/main/java/com/northern/controller/CompareXMLController.java
@@ -0,0 +1,39 @@
+package com.northern.controller;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.stream.StreamResult;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.xml.sax.SAXException;
+
+import com.northern.service.CompareXMLService;
+
+@RestController
+@RequestMapping("/xml")
+public class CompareXMLController {
+	
+	@Autowired
+	private CompareXMLService service;
+	
+	@PostMapping("/compare")
+	private ResponseEntity<?> compareXML(@RequestParam("file1") MultipartFile xml1, @RequestParam("file2") MultipartFile xml2) throws ParserConfigurationException, SAXException, IOException, TransformerException{
+		StreamResult result=service.compareXML(xml1, xml2);
+		
+		ByteArrayOutputStream baos=(ByteArrayOutputStream) result.getOutputStream();
+		String xmlContent=baos.toString("UTF-8");
+		
+		return new ResponseEntity(xmlContent,HttpStatus.OK);
+		
+	}
+}
